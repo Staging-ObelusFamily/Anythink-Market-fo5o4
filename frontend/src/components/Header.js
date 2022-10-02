f01737925f703574e2bff755703857589f5c1cdf
Shem <smagnezi8@gmail.com>
2022-10-02 09:59:45 +0000

    change

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -25,13 +25,13 @@ const LoggedInView = (props) => {
     <ul className="navbar-nav ml-auto">
       <li className="nav-item">
         <Link to="/editor" className="nav-link">
-          <i className="ion-compose"></i>&nbsp;New Item
+          <i className="ion-compose"></i>New Item
         </Link>
       </li>
 
       <li className="nav-item">
         <Link to="/settings" className="nav-link">
-          <i className="ion-gear-a"></i>&nbsp;Settings
+          <i className="ion-gear-a"></i>Settings
         </Link>
       </li>
 
